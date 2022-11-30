4600c3b9576b08fd2ca65356b8e6ede950f031d2
Thoreye6 <113397166+Thoreye6@users.noreply.github.com>
2022-11-29 20:01:01 -0500

    integrated API response for forecast and created function (new apiUrl) and lat and lon

--- script.js
+++ script.js
@@ -20,7 +20,44 @@ function showDate(date) {
   let day = days[dayIndex];
   return `${days[dayIndex]} ${hours}:${minutes}`;
 }
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
 
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+  <div class="weather-forecast" id="forecast">
+          <div class="weather-forecast-date">
+              ${day}
+            </div>
+<img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="40"/>
+<div class="weather-forecast-temperatures">
+  <span class="weather-forecast-temperature-max">
+    18°
+  </span>
+  <span class="weather-forecast-temperature-min">
+    12°
+  </span>
+</div>
+          </div>
+        </div>
+        `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "082d3d02ffdb12f2fd9b259e2ced1d0d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
 function showWeatherConditions(response) {
   console.log(response.data);
 
@@ -42,6 +79,8 @@ function showWeatherConditions(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
